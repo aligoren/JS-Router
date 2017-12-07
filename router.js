df1f7e7fe9fe8a681cff8f017a0ce8acd2230df6
Ali GOREN <goren.ali@yandex.com>
2017-12-07 15:15:36 +0300

    Update router.js

--- router.js
+++ router.js
@@ -2,29 +2,52 @@ class Router {
     constructor(el = 'body') {
         this.el = el;
         this.routeObj = {};
+        this.routeArr = [];
+        this.errorTpl = '';
     }
 
     rules(opt) {
         this.routeObj = opt;
         
+
         window.addEventListener("hashchange", (k, v) => {
+            this.routeArr = [];
             this.routeObj.forEach(el => {
                 if(el['url'] == window.location.hash) {
                     fetch(el['template'], {
                         method: 'GET'
                     }).then(resp => resp.text()).
                     then(obj => {
+                        
                         document.querySelector(this.el).innerHTML = obj;
-                    })
+                    });
+
+                    this.routeArr["ok"] = true;
                 }
             });
-        })
+            
+            if(!this.routeArr["ok"]) {
+                
+                let template = this.errorTpl;
 
+                if(!template) {
+                    template = this.routeObj[0].template;
+                }
+                
+                fetch(template, {
+                    method: 'GET'
+                }).then(resp => resp.text()).
+                then(obj => {
+                    document.querySelector(this.el).innerHTML = obj;
+                })
+            }
+        })
         return this;
     }
 
     start(route) {
         if(window.location.hash != '' && window.location.hash !== route) {
+            this.routeArr = [];
             this.routeObj.forEach(el => {
                 if(el['url'] == window.location.hash) {
                     fetch(el['template'], {
@@ -32,9 +55,27 @@ class Router {
                     }).then(resp => resp.text()).
                     then(obj => {
                         document.querySelector(this.el).innerHTML = obj;
-                    })
+                    });
+                    this.routeArr["ok"] = true;
                 }
             });
+            
+            if(!this.routeArr["ok"]) {
+                
+                let template = this.errorTpl;
+
+                if(!template) {
+                    template = this.routeObj[0].template;
+                }
+                
+                fetch(template, {
+                    method: 'GET'
+                }).then(resp => resp.text()).
+                then(obj => {
+                    document.querySelector(this.el).innerHTML = obj;
+                })
+            }
+            
         } else {
             this.routeObj.forEach(el => {
                 if(!route) {
