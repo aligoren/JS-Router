aac29843fc05dc3e501265169f29b52ea1e2e8e1
Ali GOREN <goren.ali@yandex.com>
2017-12-12 10:32:28 +0300

    changed sample

--- custom.js
+++ custom.js
@@ -4,13 +4,27 @@ $router.errorTpl = '404.html';
 $router.rules(
     [
         {   'url': '#!',
-            'template': 'hash.html'
+            'template': 'hash.html',
+            'fn': function() {
+                $router.data.window.title = 'Start Page';
+            }
         },
         {   'url': '#/hash1',
-            'template': 'hash1.html'
+            'template': 'hash1.html',
+            'fn': function() {
+                $router.data.window.title = 'Hash1 Page';
+            }
         },
         {   'url': '#/hash2',
             'template': 'hash2.html'
+        },
+        {   
+            'url': '#/hash3',
+            'template': 'hash3.html'
+        },
+        {
+            'url': '#/success',
+            'template': 'success.html'
         }
     ]
 ).start('#!')
